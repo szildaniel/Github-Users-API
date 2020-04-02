dce8bbc2d63f99842d9fb66b82082d89eb670ad8
Daniel Mydlarz <szildaniel@gmail.com>
2020-04-02 21:33:43 +0200

    added some hover styles to repo components

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAsync } from "react-async-hook";
 
@@ -7,27 +7,44 @@ const fetchGithubRepos = async url => (await fetch(`${url}`)).json();
 const Repo = props => {
   const githubRepos = useAsync(fetchGithubRepos, [props.url]);
 
-  return (
-    typeof(props.url)  === 'undefined' ? <h3 className="empty__input">That is place for user repositories, please enter username to see user repos.</h3> :
+  return typeof props.url === "undefined" ? (
+    <h3 className="empty__input">
+      That is place for user repositories, please enter username to see user
+      repos.
+    </h3>
+  ) : (
     <div>
       <h2>Repositories</h2>
       {githubRepos.loading && <div>Loading</div>}
       {githubRepos.error && <div>Error: {githubRepos.error.message}</div>}
-      {githubRepos.result && ( 
-        githubRepos.result.map( (repo, i) => 
-        <div className="repoContainer" key={i}>
-          <h3>{repo.name}</h3>
-          <p>{repo.language}</p>
-          <a className="repoLink" target="_blank" disabled={!props.url} href={repo.html_url}>
-            <FontAwesomeIcon
-              color={props.theme === "dark" ? "white" : "black"}
-              icon="link"
-            />
-          </a>
-        </div>
-      )
-        
-      )}
+      {githubRepos.result &&
+        githubRepos.result.map((repo, i) => (
+          <div
+            className={
+              props.theme === "dark"
+                ? "repoContainer"
+                : "repoContainer lightTheme"
+            }
+            key={i}
+          >
+            <h3>{repo.name}</h3>
+            <p>
+              {repo.language}
+              <a
+                className="repoLink"
+                target="_blank"
+                rel="noopener noreferrer"
+                disabled={!props.url}
+                href={repo.html_url}
+              >
+                <FontAwesomeIcon
+                  color={props.theme === "dark" ? "white" : "black"}
+                  icon="link"
+                />
+              </a>
+            </p>
+          </div>
+        ))}
     </div>
   );
 };
