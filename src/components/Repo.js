209821dee9f0892ccf80c37958380f317654fc65
Daniel Mydlarz <szildaniel@gmail.com>
2020-04-05 12:05:20 +0200

    in Repo.js i added function that reduce all user repositories to random 4 repo, then add it to state and display, just 4 repo instead of 30

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAsync } from "react-async-hook";
+import randomInt from '../helper';
 
 
 const fetchGithubRepos = async url => (await fetch(`${url}`)).json();
 
-const Repo = props => {
-  const githubRepos = useAsync(fetchGithubRepos, [props.url]);
+function useRandomUserRepos(url, options) {
+  const [allRepos, setAllRepos] = useState();
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [reducedRepos, setReducedRepos] = useState();
 
-  return typeof props.url === "undefined" ? (
+  useEffect( () => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setAllRepos(json);
+        setIsLoading(false);
+      } 
+      catch (error) {
+        setError(error);
+     }
+    }; fetchData();
+  }, [url])
+
+  useEffect( () => {
+    const reduceAllRepos = arr => {
+     const fourRepos = [];
+      if(arr && arr.length > 3){
+        const arrLength = arr.length;
+
+        for(let i=0; i<=3; i++){
+          const randomIndex = randomInt(0, arrLength);
+          fourRepos.push(arr[randomIndex]);
+        }
+      };
+      setReducedRepos(fourRepos);
+    }; 
+    reduceAllRepos(allRepos);
+  }, [allRepos])
+   
+
+  return { allRepos, error, isLoading, reducedRepos }
+}
+
+const Repo = props => {
+const githubRepos = useRandomUserRepos(props.url, {})
+  
+  return typeof githubRepos.allRepos === 'undefined' ? (
     <h3 className="empty__input">
       That is place for user repositories, please enter username to see user
       repos.
@@ -16,10 +58,10 @@ const Repo = props => {
   ) : (
     <div>
       <h2>Repositories</h2>
-      {githubRepos.loading && <div>Loading</div>}
+      {githubRepos.isLoading && <div>Loading</div>}
       {githubRepos.error && <div>Error: {githubRepos.error.message}</div>}
-      {githubRepos.result &&
-        githubRepos.result.map((repo, i) => (
+      {githubRepos.reducedRepos &&
+        githubRepos.reducedRepos.map((repo, i) => (
           <div className={props.theme === "dark"
                 ? "repoContainer"
                 : "repoContainer lightTheme"}
@@ -47,4 +89,5 @@ const Repo = props => {
     </div>
   );
 };
+
 export default Repo;
